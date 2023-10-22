5d4c22f0f00dc3a5fba909eed974d1cb80c4f92a
{AlexDaile} <{gebizovoleksandr@gmail.com}>
2023-10-22 20:58:15 +0300

    first commit

---
+++ Main.cs
@@ -0,0 +1,22 @@
+﻿namespace laba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ввод довжин сторін прямокутника
+            double side1, side2;
+            Console.WriteLine("Введіть довжину першої сторони: ");
+            side1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть довжину другої сторони: ");
+            side2 = double.Parse(Console.ReadLine());
+
+            // Утворення об'єкта класу Rectangle
+            Rectangle rectangle = new Rectangle(side1, side2);
+
+            // Виведення на екран
+            Console.WriteLine("Периметр прямокутника: {0}", rectangle.Perimeter);
+            Console.WriteLine("Площа прямокутника: {0}", rectangle.Area);
+        }
+    }
+}
---
+++ laba3_part2/Figure.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace laba3_part2
+{
+    public class Figure
+    {
+        private Point[] points;
+
+        public Figure(Point[] points)
+        {
+            this.points = points;
+        }
+
+        public double GetSideLength(Point A, Point B)
+        {
+            return Math.Sqrt((A.X - B.X) * (A.X - B.X) + (A.Y - B.Y) * (A.Y - B.Y));
+        }
+
+        public void CalculatePerimeter()
+        {
+            // Обчислення периметра многокут
+            double perimeter = 0;
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                perimeter += GetSideLength(points[i], points[i + 1]);
+            }
+            perimeter += GetSideLength(points[points.Length - 1], points[0]);
+
+            // Виведення на консоль
+            Console.WriteLine("Периметр многокутника: {0}", perimeter);
+        }
+    }
+}
